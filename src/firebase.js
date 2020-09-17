4f4d407be41a681a178b37bc78499a59081a206d
Chinwike1 <mrchinwike@gmail.com>
2020-09-17 09:18:53 +0100

    Installed dotenv package and put firebase credentials in env file

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDaBQbzhDCqcVVBxEhPHQ051MF-bxeqjyw',
-  authDomain: 'diarysessions-blog.firebaseapp.com',
-  databaseURL: 'https://diarysessions-blog.firebaseio.com',
-  projectId: 'diarysessions-blog',
-  storageBucket: 'diarysessions-blog.appspot.com',
-  messagingSenderId: '516010216568',
-  appId: '1:516010216568:web:06aa6ca0e4783febbff18d',
-  measurementId: 'G-LTGWMCN4YH',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_BASEURL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
